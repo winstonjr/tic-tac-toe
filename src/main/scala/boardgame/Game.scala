4ae0d2f5dac741924862a14a0af7c7e86cba3a46
Winston Pacheco Junior <winston@atleastonce.cc>
2021-04-02 12:19:33 -0300

    put the responsibility to get the positions to play for the player

--- src/main/scala/boardgame/Game.scala
+++ src/main/scala/boardgame/Game.scala
@@ -15,12 +15,7 @@ object Game extends App {
   do {
     ticTacToeMatch.printBoard()
 
-    print(s"${ticTacToeMatch.turnName} escolha a linha: ")
-    val row = readInt()
-    print(s"${ticTacToeMatch.turnName} escolha a Coluna: ")
-    val col = readInt()
-
-    ticTacToeMatch.play(row, col) match {
+    ticTacToeMatch.play() match {
       case Left(bla) => println(bla)
       case default => default
     }
@@ -41,8 +36,10 @@ case class Game(playerXName: String, playerOName: String) {
   def boardStatus: Either[String, String] = board.boardStatus()
   def printBoard(): Unit = board.printBoard()
 
-  def play(row: Int, col: Int): Either[String, String] = {
-    board.stamp(turn.symbol, row, col) match {
+  def play(): Either[String, String] = {
+    val positions = turn.getPositions
+
+    board.stamp(turn.symbol, positions._1, positions._2) match {
       case Left(bla) => Left(bla)
       case Right(ble) =>
         turn = if (turn.equals(playerO)) playerX else playerO
@@ -52,10 +49,11 @@ case class Game(playerXName: String, playerOName: String) {
 }
 
 trait PlayerBoard {
-  val board: Array[Int]
   val symbol: String
+  val name: String
   final val defaultEmpty = 0
   protected final val boardSize = 3
+  val board: Array[Int] = Array.fill(boardSize*boardSize)(defaultEmpty)
 
   /**
    * validates if is a winning board
@@ -98,10 +96,18 @@ trait PlayerBoard {
 
   def getBoardArray: Array[String] =
     board.map(v => if (1.equals(v)) symbol else " ")
+
+  def getPositions: (Int, Int) = {
+    print(s"$name escolha a linha: ")
+    val row = readInt()
+    print(s"$name escolha a Coluna: ")
+    val col = readInt()
+
+    (row, col)
+  }
 }
 
 case class Player(symbol: String, name: String) extends PlayerBoard {
-  val board: Array[Int] = Array.fill(boardSize*boardSize)(defaultEmpty)
 }
 
 case class Board(playerX: Player, playerO: Player) {
@@ -166,57 +172,4 @@ case class Board(playerX: Player, playerO: Player) {
       }).mkString("|")).mkString("\n---+---+---\n") + "\n")
     println()
   }
-
-
-  //  private def isWinningBoard(board: Array[Int]): Boolean = {
-  //    // 000000111 = 7   = Row 3
-  //    // 000111000 = 56  = Row 2
-  //    // 111000000 = 448 = Row 1
-  //    // 001001001 = 73  = Column 3
-  //    // 010010010 = 146 = Column 2
-  //    // 100100100 = 292 = Column 1
-  //    // 100010001 = 273 = Diagonal
-  //    // 001010100 = 84  = Inverse Diagonal
-  //    val winningPlays: Seq[Int] = Seq(7, 56, 448, 73, 146, 292, 273, 84)
-  //
-  //    winningPlays.exists(v => {
-  //      (Integer.parseInt(board.mkString(""), 2) & v) == v
-  //    })
-  //
-  //    // https://loganmeetsworld.com/2017/10/15/using-of-bitwise-operators.html
-  //    // https://github.com/loganmeetsworld/js-tic-tac-toe/blob/lm/master/tic-tac-toe.js
-  //  }
-
-  //  val testBoards: Array[Array[Int]] = Array(
-  //    // Straight Win
-  //    Array[Int](0, 0, 0, 0, 0, 0, 1, 1, 1),
-  //    Array[Int](0, 0, 0, 1, 1, 1, 0, 0, 0),
-  //    Array[Int](1, 1, 1, 0, 0, 0, 0, 0, 0),
-  //    Array[Int](0, 0, 1, 0, 0, 1, 0, 0, 1),
-  //    Array[Int](0, 1, 0, 0, 1, 0, 0, 1, 0),
-  //    Array[Int](1, 0, 0, 1, 0, 0, 1, 0, 0),
-  //    Array[Int](1, 0, 0, 0, 1, 0, 0, 0, 1),
-  //    Array[Int](0, 0, 1, 0, 1, 0, 1, 0, 0),
-  //    // Win
-  //    Array[Int](0, 1, 0, 0, 0, 0, 1, 1, 1),
-  //    Array[Int](0, 0, 0, 1, 1, 1, 0, 0, 1),
-  //    Array[Int](1, 1, 1, 0, 0, 1, 0, 0, 0),
-  //    Array[Int](0, 0, 1, 1, 0, 1, 0, 0, 1),
-  //    Array[Int](0, 1, 1, 0, 1, 0, 0, 1, 0),
-  //    Array[Int](1, 0, 1, 1, 0, 0, 1, 0, 0),
-  //    Array[Int](1, 0, 0, 0, 1, 0, 1, 0, 1),
-  //    Array[Int](0, 0, 1, 0, 1, 0, 1, 0, 1),
-  //    // loss
-  //    Array[Int](1, 1, 0, 1, 0, 0, 0, 0, 0),
-  //    Array[Int](0, 0, 0, 1, 0, 1, 0, 0, 0),
-  //    Array[Int](1, 0, 1, 0, 0, 0, 0, 0, 0),
-  //    Array[Int](0, 0, 1, 0, 0, 0, 0, 0, 1),
-  //    Array[Int](0, 0, 0, 0, 1, 0, 0, 1, 0),
-  //    Array[Int](0, 0, 0, 1, 0, 0, 1, 0, 0),
-  //    Array[Int](1, 0, 0, 0, 1, 0, 0, 0, 0),
-  //    Array[Int](0, 0, 1, 0, 1, 0, 0, 0, 0),
-  //
-  //    Array[Int](1, 0, 0, 0, 0, 0, 0, 0, 0)
-  //  )
-  //  //testBoards.foreach(board => println(s"Ganhou mizerávi? ${if (winningBoard(board)) "Sim =D" else "Não ='/"}"))
 }
